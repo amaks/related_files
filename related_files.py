1416f0c185e5eec43f7a1ad2a244b580d37279c0
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-11-16 20:08:31 +0200

    refactoring

--- related_files.py
+++ related_files.py
@@ -1,5 +1,6 @@
 import os, sublime, sublime_plugin, glob
-# super+alt+6
+# super + alt + 9
+
 class RelatedFilesCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
@@ -10,20 +11,15 @@ class RelatedFilesCommand(sublime_plugin.TextCommand):
     files             = []
 
     if 'controllers' in source_path:
-      only_file_name = file_name.split('/')[-1].split('_controller.rb')[0]
-      if file_name.split('/')[-2] != 'controllers':
-        files_source_path = rails_view_path.replace('controllers', 'views/') + file_name.split('/')[-2] + '/' + only_file_name +'/'
-      else:
-        files_source_path = rails_view_path + '/views/' + only_file_name +'/'
+      controller_file   = None
+      files_source_path = self.views_file_path(file_name, rails_view_path)
+    elif 'models' in source_path:
+      files_source_path, controller_file = self.view_related_files(file_name, rails_view_path)
     else:
-      only_file_name    = source_path.split('/')[-1]
       files_source_path = source_path
+      controller_file   = self.controller_file_path(file_name, rails_view_path, source_path)
 
-      if file_name.split('/')[-1] != 'views':
-        controller_file   = rails_view_path.replace('views', 'controllers') + '/' + only_file_name + '_controller.rb'
-      else:
-        controller_file   = rails_view_path + '/controllers/' + only_file_name + '_controller.rb'
-
+    if controller_file is not None:
       if os.path.isfile(controller_file):
         files.append(controller_file)
 
@@ -42,6 +38,33 @@ class RelatedFilesCommand(sublime_plugin.TextCommand):
 
     self.files = files
     sublime.active_window().show_quick_panel(files, self.open_file)
+
+  def view_related_files(self, file_name, rails_view_path):
+    only_file_name = file_name.split('/')[-1].split('.rb')[0]
+
+    files_source_path = rails_view_path + '/views/' + only_file_name +'s/'
+    controller_file   = rails_view_path + '/controllers/' + only_file_name + 's_controller.rb'
+
+    return files_source_path, controller_file
+
+  def controller_file_path(self, file_name, rails_view_path, source_path):
+    only_file_name = source_path.split('/')[-1]
+    if file_name.split('/')[-1] != 'views':
+      controller_file = rails_view_path.replace('views', 'controllers') + '/' + only_file_name + '_controller.rb'
+    else:
+      controller_file = rails_view_path + '/controllers/' + only_file_name + '_controller.rb'
+
+    return controller_file
+
+  def views_file_path(self, file_name, rails_view_path):
+    only_file_name = file_name.split('/')[-1].split('_controller.rb')[0]
+
+    if file_name.split('/')[-2] != 'controllers':
+      files_source_path = rails_view_path.replace('controllers', 'views/') + file_name.split('/')[-2] + '/' + only_file_name +'/'
+    else:
+      files_source_path = rails_view_path + '/views/' + only_file_name +'/'
+
+    return files_source_path
 
   def open_file(self, index):
     if index >= 0:
